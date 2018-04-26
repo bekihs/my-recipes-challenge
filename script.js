6996344f3e0a265d412f1c9a99d7859f706e82c4
dsalvat596 <dsalvat596@gmail.com>
2018-04-26 10:33:54 +0300

    challenge commit

--- script.js
+++ script.js
@@ -20,63 +20,82 @@ var RecipeApp = function () {
     //id's for ingredients
     var ingId = 0;
 
-    var createRecipe = function(name, image){
+    var createRecipe = function (name, image) {
         var recipe = {
             name: name,
-            image: image, 
+            image: image,
             ingredients: [],
             id: recId
         };
 
         //keeps recipe ids unique 
-        recId ++; 
+        recId++;
 
         recipes.push(recipe);
     };
 
-    var createIngredients = function(){
+    var createIngredients = function (ingr, recipId) {
         //add code
+        var recipe = _getIngredients(recipId)
+
+        var ingrObj = {
+            name: ingr,
+            id: ingId
+        }
+        recipe.ingredients.push(ingrObj);
+        console.log(recipes);
+        ingId += 1;
+
+
     };
 
-    var _getIngredients = function(recipe){
+    var _getIngredients = function (recipe) {
         //add code
-        return "";
+        for (var i = 0; i < recipes.length; i++) {
+            if (recipes[i].id === recipe) {
+                return recipes[i];
+            }
+        }
+
     };
 
     var renderRecipes = function () {
         //empty recipes div
         $recipes.empty();
 
-        for(var i = 0; i < recipes.length; i ++){
+        for (var i = 0; i < recipes.length; i++) {
             //current recipe in iteration
             var recipe = recipes[i];
 
             //return HTML for all ingredients
-            var ingredients = _getIngredients(); //add code
-
+            var ingredients = _getIngredients(recipe); //add code
+            var ingredientsRend = '<li>' + ingredients + '</li>';
+            //**************** STUCK HERE***********  trying to render the ingredients, it renders an LI of "undefined"
             $recipes.append(
-                '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' + 
-                    '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
-                    '<img class="recipe-img" src="' + recipe.image + '"/>' +
-                    '<hr>' +
-                    '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
-                    '<div class="input-group mb-3">' +
-                        '<div class="input-group-prepend">' +
-                            '<span class="add-ingredients input-group-text" id="basic-addon3">Add Ingredients</span>' +
-                        '</div>' + 
-                        '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
-                        
-                    '</div>' +
-                    '<ul class="ingredients">' + ingredients + '</ul>'+
+                '<div class="recipe col-md-6  offset-md-3 img-fluid shadow" data-id="' + recipe.id + '">' +
+                '<h4 class="text-capitalize font-italic text-center">' + recipe.name + '</h4>' +
+                '<img class="recipe-img" src="' + recipe.image + '"/>' +
+                '<hr>' +
+                '<h5 class="font-italic font-bold text-center">ingredients</h5>' +
+                '<div class="input-group mb-3">' +
+                '<div class="input-group-prepend">' +
+                '<span class="add-ingredients input-group-text" id="basic-addon3"><button class="btn btn-primary add-ingredient">Add Ingredients</button></span>' +
+                '</div>' +
+                '<input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">' +
+
+                '</div>' +
+                '<ul class="ingredients">'+ ingredientsRend + '</ul>' +
                 '</div>'
             );
         }
     };
 
+
+
     return {
         createRecipe: createRecipe,
         renderRecipes: renderRecipes,
-        // createIngredients: createIngredients
+        createIngredients: createIngredients
     }
 };
 
@@ -86,13 +105,21 @@ var app = RecipeApp();
 //--------EVENTS
 
 //add a recipe
-$('.add-recipe').on('click', function(){
+$('.add-recipe').on('click', function () {
     //collect input text
     var name = $('#recipe-name').val();
     var image = $('#recipe-image').val();
+
 
     //add recipe to array and render
     app.createRecipe(name, image);
     app.renderRecipes();
 });
 
+//add ingredients
+$('.recipes').on('click', '.add-ingredient', function () {
+    var ingredientToAdd = $('#basic-url').val();
+    var recipId = $(this).closest('.recipe').data().id;
+    app.createIngredients(ingredientToAdd, recipId);
+   // console.log(ingredientToAdd, recipId);
+});
